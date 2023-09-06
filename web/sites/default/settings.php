93a7da7aa4a124f3cc869131431d5e8a950c5961
hw <hussainweb@gmail.com>
2023-09-06 16:47:39 -0400

    fix: revert hash_salt settings

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -264,7 +264,7 @@ $databases = [];
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = '';
+# $settings['hash_salt'] = '';
 
 /**
  * Deployment identifier.
