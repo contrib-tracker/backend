8478f6f48ad590e08d943431446410fcf5bb160f
hw <hussainweb@gmail.com>
2023-10-24 11:38:10 -0400

    chore: update eslint and make it compatible with gulp

--- web/themes/custom/contribtracker/gulp-tasks/lint.js
+++ web/themes/custom/contribtracker/gulp-tasks/lint.js
@@ -1,5 +1,5 @@
 const stylelint = require('gulp-stylelint');
-const eslint = require('gulp-eslint');
+const eslint = require('gulp-eslint-new');
 const gulpIf = require('gulp-if');
 
 module.exports = (gulp, config) => {
