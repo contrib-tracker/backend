765a5bd331000cc384ec6a75bc6df25267d74543
zeshanziya <zeshan.ziya@axelerant.com>
2024-05-15 07:39:06 +0530

    add vr_settings.local.php

--- .github/ci.sh
+++ .github/ci.sh
@@ -4,7 +4,7 @@ dir=$(dirname $0)
 
 set -ex
 
-cp ${dir}/../.gitlab/settings.local.php ${dir}/../web/sites/default/settings.local.php
+cp ${dir}/vr_settings.local.php ${dir}/../web/sites/default/settings.local.php
 
 sed -ri -e "s!/var/www/html/web!$GITHUB_WORKSPACE/web!g" /etc/apache2/sites-available/*.conf
 sed -ri -e "s!/var/www/html/web!$GITHUB_WORKSPACE/web!g" /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf
