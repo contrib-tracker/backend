7390d70bc54a116817d15d136617cfb3adc14157
hw <hussainweb@gmail.com>
2024-05-16 07:46:43 -0400

    revert: partial change from #445
    this config breaks with Cypress 13. It fails to detect the baseUrl entirely. Whereas directly setting the `e2e.baseUrl` works with both Cypress 12 and 13.

--- web/themes/custom/contribtracker/cypress.config.js
+++ web/themes/custom/contribtracker/cypress.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      baseUrl: 'https://contribtracker.ddev.site/'
-    },
+    baseUrl: 'https://contribtracker.ddev.site/',
   },
 });
