b11752820bfb9730a48e027047ed466b34edac88
zeshanziya <zeshan.ziya@axelerant.com>
2024-05-29 09:53:42 +0530

    add action for deleting platform.sh env

---
+++ .github/actions/platformsh-clean-env/script.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "PR_REF is $PR_REF"
+
+TYPE=$(platform environment:info --project $PLATFORM_PROJECT_ID --environment $PR_REF type)
+NAME=$(platform environment:info --project $PLATFORM_PROJECT_ID --environment $PR_REF name)
+
+
+echo "Enviroment type is $TYPE"
+echo "Environment name is $NAME"
+
+if [ "$TYPE" = "development" ] && [ -n "$NAME" ]; then
+  echo "Deactivating the env on platform.sh"
+  platform environment:delete --project $PLATFORM_PROJECT_ID $PR_REF -y
+else
+  echo "No env found for the PR on Platform.sh"
+fi
