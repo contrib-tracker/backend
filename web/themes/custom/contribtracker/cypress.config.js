a8ad90c0c5f71044a1f479518d65dff07bb516b8
Yemaneberhan-Lemma <yemaneberhan.lemma@axelerant.com>
2024-10-21 23:15:22 +0300

    Added major Tagged Test Filter configurations

--- web/themes/custom/contribtracker/cypress.config.js
+++ web/themes/custom/contribtracker/cypress.config.js
@@ -6,5 +6,8 @@ export default defineConfig({
   video: process.env.CYPRESS_VIDEO !== 'false', // Defaults to true unless explicitly set to 'false'
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL || 'https://contrib.axelerant.com', // Default to prod url if no base URL is set
+    env: {
+      grepTags: ''
+    }
   },
 });
