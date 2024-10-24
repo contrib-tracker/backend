91421199825a97af36bd34fb185138402f606eee
KosalaiV <101544305+KosalaiV@users.noreply.github.com>
2024-10-24 23:20:18 +0530

    Cypress: Added tests and updated config.js (#583)
    ---------
    
    Co-authored-by: KOSALAI V <kosalaiv@KOSALAIs-MacBook-Pro.local>
    Co-authored-by: zeshanziya <zeshan.ziya@axelerant.com>

---
+++ web/themes/custom/contribtracker/cypress/e2e/form-reset-button-check.cy.js
@@ -0,0 +1,85 @@
+// Describe the test suite for verifying form reset functionality with positive and negative scenarios
+describe('Form Reset Button Validation with Positive and Negative Scenarios', () => {
+  // Before each test, visit the page containing the form
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  // Test to validate form reset functionality with valid data entry
+  it('should reset the form with valid data entry', () => {
+    // Extract the first user name from the specified locator
+    cy.get('.contrib__card_contributer > a')
+      .first() // Select the first user
+      .invoke('text') // Get the text of the user
+      .then((userName) => {
+        // Log the extracted user name for debugging
+        cy.log(`Extracted user name: ${userName.trim()}`);
+
+        // Type the extracted user name into the input field
+        cy.get('#edit-uid').type(userName.trim());
+
+        // Click the apply button to filter results
+        cy.get('#edit-submit-all-contributions')
+          .should('have.value', 'Apply')
+          .click();
+
+        // Verify the input field contains the entered user name
+        cy.get('#edit-uid').should('have.value', userName.trim());
+
+        // Click the reset button to clear the form
+        cy.get('#edit-reset-all-contributions')
+          .should('have.value', 'Reset')
+          .click();
+
+        // Verify the input field is reset to empty
+        cy.get('#edit-uid').should('have.value', '');
+
+        // Verify the reset button visibility
+        verifyResetButtonVisibility();
+      });
+  });
+
+  // Test to validate form reset functionality with invalid data entry
+  it('should reset the form with invalid data entry', () => {
+    // Type an invalid user name into the input field
+    cy.get('#edit-uid').type('NonExistUser'); // Enter an invalid user name
+
+    // Click the apply button to filter results
+    cy.get('#edit-submit-all-contributions')
+      .should('have.value', 'Apply')
+      .click();
+
+    // Verify the input field contains the entered invalid user name
+    cy.get('#edit-uid').should('have.value', 'NonExistUser');
+
+    // Click the reset button to clear the form
+    cy.get('#edit-reset-all-contributions')
+      .should('have.value', 'Reset')
+      .click();
+
+    // Verify the input field is reset to empty
+    cy.get('#edit-uid').should('have.value', 'NonExistUser');
+
+    // Reset filter does not reset view exposed forms if there are validation error.
+    cy.get('div[aria-label="Error message"]')
+      .should('be.visible')
+      .contains(`There are no users matching "NonExistUser"`);
+
+    // Verify the reset button visibility
+    // verifyResetButtonVisibility();
+  });
+});
+
+// Function to verify the reset button visibility after reset
+function verifyResetButtonVisibility() {
+  cy.get('body').then(($body) => {
+    if ($body.find('#edit-reset-all-contributions').length > 0) {
+      throw new Error('Reset Button is still visible, expected to not exist');
+    } else {
+      assert.isOk(
+        'Reset Button not found - All works as expected',
+        'everything is OK',
+      );
+    }
+  });
+}
---
+++ web/themes/custom/contribtracker/cypress/e2e/page-sublink-check.cy.js
@@ -0,0 +1,58 @@
+// Describe the test suite for Main Page Sublinks Response Code Validation
+describe('Main Page Sublinks Response Code Validation', () => {
+  // Before each test, visit the main page
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  // Test case to validate the response codes for header links
+  it('should validate the response codes for header links', () => {
+    validateResponseCodes('header a');
+  });
+
+  // Test case to validate the response codes for main content links
+  it('should validate the response codes for main links', () => {
+    //Login to check admin accessibile pages
+    cy.login('admin');
+    validateResponseCodes('main a');
+  });
+
+  // Test case to validate the response codes for footer links
+  it('should validate the response codes for footer links', () => {
+    validateResponseCodes('footer a');
+  });
+
+  // Function to validate response codes for links in the given section
+  function validateResponseCodes(sectionSelector) {
+    // Array to store URLs that failed validation
+    const failedUrls = [];
+
+    // Collect all links from the specified section and validate each link
+    cy.get(sectionSelector)
+      .each(($el) => {
+        const href = $el.attr('href');
+        if (href) {
+          // Make a request to the URL and validate the response code
+          cy.request({
+            url: href,
+            failOnStatusCode: false, // Allow the test to continue even if the status code is not 2xx or 3xx
+          }).then((response) => {
+            // If the response status code is not 200, 301, or 302, add it to the failedUrls array
+            if (![200, 301, 302].includes(response.status)) {
+              failedUrls.push({ link: href, status: response.status });
+            }
+          });
+        }
+      })
+      .then(() => {
+        // After all requests are done, check if there are any failed URLs
+        if (failedUrls.length > 0) {
+          // Log and fail the test with the details of failed URLs
+          const error = new Error(
+            `Failed URLs:\n${failedUrls.map((url) => `Link: ${url.link}, Status: ${url.status}`).join('\n')}`,
+          );
+          throw error;
+        }
+      });
+  }
+});
