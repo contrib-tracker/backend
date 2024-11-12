4d68e5e669a2d7e4c13ca48616c259de769e0cfa
hw <hussainweb@gmail.com>
2024-11-11 19:37:44 -0500

    fix: phpcs violations

--- web/modules/custom/ct_user/src/Plugin/Block/ContribGraph.php
+++ web/modules/custom/ct_user/src/Plugin/Block/ContribGraph.php
@@ -5,14 +5,14 @@ namespace Drupal\ct_user\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 
 /**
-* Provides a block with a contribution commit graph.
-*
-* @Block(
-*   id = "contrib_graph_blocl",
-*   admin_label = @Translation("Contribution commit graph"),
-*   category = "Custom"
-* )
-*/
+ * Provides a block with a contribution commit graph.
+ *
+ * @Block(
+ *   id = "contrib_graph_blocl",
+ *   admin_label = @Translation("Contribution commit graph"),
+ *   category = "Custom"
+ * )
+ */
 class ContribGraph extends BlockBase {
 
   /**
