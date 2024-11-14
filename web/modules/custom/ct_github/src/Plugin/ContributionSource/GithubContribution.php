6212d17e7f2d17d30c6432540ecf8bc9b085928c
Yemaneberhan-Lemma <yemaneberhan.lemma@axelerant.com>
2024-11-14 16:39:26 +0300

    When token is not available, stop ct_github processes

--- web/modules/custom/ct_github/src/Plugin/ContributionSource/GithubContribution.php
+++ web/modules/custom/ct_github/src/Plugin/ContributionSource/GithubContribution.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Drupal\ct_github\Plugin\ContributionSource;
 
 use Drupal\Component\Plugin\PluginBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\ct_github\GithubQuery;
@@ -44,6 +45,10 @@ class GithubContribution extends PluginBase implements ContributionSourceInterfa
    * @var \Drupal\ct_github\GithubQuery
    */
   protected GithubQuery $query;
+  /**
+   * GitHub Token Validity
+   */
+  protected bool $tokenValid;
 
   /**
    * Constructs a Drupal\rest\Plugin\ResourceBase object.
@@ -59,10 +64,16 @@ class GithubContribution extends PluginBase implements ContributionSourceInterfa
    * @param \Drupal\ct_github\GithubQuery $query
    *   The injected github query service.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entityTypeManager, GithubQuery $query) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entityTypeManager, GithubQuery $query, ConfigFactoryInterface $configFactory) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->entityTypeManager = $entityTypeManager;
     $this->query = $query;
+    $this->configFactory = $configFactory;
+    /**
+     * Check Github token availability and execute
+     */
+    $token = $this->configFactory->get('ct_github.settings')->get('github_auth_token');
+    $this->tokenValid = (strlen($token) >= 40);
   }
 
   /**
@@ -74,7 +85,8 @@ class GithubContribution extends PluginBase implements ContributionSourceInterfa
       $plugin_id,
       $plugin_definition,
       $container->get('entity_type.manager'),
-      $container->get('ct_github.query')
+      $container->get('ct_github.query'),
+      $container->get('config.factory')
     );
   }
 
@@ -82,6 +94,9 @@ class GithubContribution extends PluginBase implements ContributionSourceInterfa
    * Get all users with github username.
    */
   public function getUsers() {
+    if (!$this->tokenValid) {
+      return [];
+    }
     $uids = $this->entityTypeManager->getStorage('user')->getQuery()
       ->accessCheck(TRUE)
       ->condition('field_github_username', '', '!=')
@@ -95,6 +110,9 @@ class GithubContribution extends PluginBase implements ContributionSourceInterfa
    * {@inheritdoc}
    */
   public function isUserValid(User $user): bool {
+    if (!$this->tokenValid) {
+      return FALSE;
+    }
     $username = $user->field_github_username[0]->getValue()['value'];
     return $this->query->isUserValid($username);
   }
@@ -103,6 +121,9 @@ class GithubContribution extends PluginBase implements ContributionSourceInterfa
    * Returns a user retriever object.
    */
   protected function getOrCreateRetriever(User $user): GithubRetriever {
+    if (!$this->tokenValid) {
+      return NULL;
+    }
     $username = $user->field_github_username[0]->getValue()['value'];
     if (isset($this->retriever[$username])) {
       return $this->retriever[$username];
@@ -115,6 +136,9 @@ class GithubContribution extends PluginBase implements ContributionSourceInterfa
    * {@inheritdoc}
    */
   public function getUserIssues(User $user) {
+    if (!$this->tokenValid) {
+      return [];
+    }
     return $this->getOrCreateRetriever($user)->getIssues();
   }
 
@@ -122,6 +146,9 @@ class GithubContribution extends PluginBase implements ContributionSourceInterfa
    * {@inheritdoc}
    */
   public function getUserCodeContributions(User $user) {
+    if (!$this->tokenValid) {
+      return [];
+    }
     return $this->getOrCreateRetriever($user)->getCodeContributions();
   }
 
