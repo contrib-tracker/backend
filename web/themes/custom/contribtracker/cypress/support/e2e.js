dacf580ea0c2159049aa1860737046f1ec7d40ea
Yemaneberhan-Lemma <yemaneberhan.lemma@axelerant.com>
2024-12-02 23:47:40 +0300

    Added more Cypress test scripts

--- web/themes/custom/contribtracker/cypress/support/e2e.js
+++ web/themes/custom/contribtracker/cypress/support/e2e.js
@@ -1,5 +1,6 @@
 import './commands';
 import 'cypress-real-events'; // cypress inbuilt trigger() will only affect events in JavaScript and will not trigger any effects in CSS. See for more details: https://docs.cypress.io/api/commands/hover
-
+import 'cypress-axe';
+import 'cypress-file-upload';
 import registerCypressGrep from '@cypress/grep';
 registerCypressGrep();
