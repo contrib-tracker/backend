2171d47ef1bf5cf7a4c0a76a8122ce5959197eea
Yemaneberhan-Lemma <yemaneberhan.lemma@axelerant.com>
2024-12-20 16:30:32 +0300

    Fixing eslint ignore issue

---
+++ web/eslint.config.mjs
@@ -0,0 +1,13 @@
+
+export default {
+  ignores: [
+    'core/**/*',
+    'vendor/**/*',
+    'sites/**/*/files/**/*',
+    'libraries/**/*',
+    'sites/**/*/libraries/**/*',
+    'profiles/**/*/libraries/**/*',
+    '**/js_test_files/**/*',
+    '**/node_modules/**/*',
+  ],
+};
